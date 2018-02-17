998c5a3479e6eed94134c0ef4a02aa2f61620e91
AriellaRogueN <arrodriguezn@gmail.com>
2018-02-17 02:48:08 -0300

    Agregando Collapse-estilizando Documento

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+//Variables HTML
 const form = document.getElementById("search-form");
 const searchPokemon = document.getElementById("pokemon-search");
 const insert = document.getElementById("insertPokemon");
@@ -11,7 +12,7 @@ form.addEventListener('submit', function (e) {
     getPokemon();
     
 });
-
+//LLamando datos desde API
 function getPokemon() {
     const pokemonRequest = new XMLHttpRequest();
     pokemonRequest.open('GET', `https://pokeapi.co/api/v2/pokemon/${searchedPokemon}`);
@@ -23,12 +24,16 @@ function getPokemon() {
 function handleError() {
     console.log('Se ha presentado un error');
 }
-
+//Variables API-Funcion Principal
 function addPokemon() {
     const data = JSON.parse(this.responseText);
     console.log(data);
     const name = data.name
     console.log(name)
+    const ability=data.abilities[0].ability.name
+    console.log(ability)
+    const otherability=data.abilities[1].ability.name
+    console.log(otherability)
     const image = data.sprites.front_default
     console.log(image)
     const type = data.types[0].type.name
@@ -46,14 +51,44 @@ function addPokemon() {
     newImage.setAttribute('src', image);
     newImage.setAttribute('class', 'pknImg')
     const figcaption = document.createElement('figcaption')
+    figcaption.setAttribute('class','caption');
     const named = document.createTextNode(name)
+    const view = document.createElement('p');
+    view.setAttribute('data-toggle', 'collapse')
+    view.setAttribute('data-target', '#collapsePokemon')
+    const more = document.createTextNode('ver más')
+    const collapse = document.createElement('div');
+    collapse.setAttribute('class','collapse');
+    collapse.setAttribute('id', 'collapsePokemon');
+    const well = document.createElement('div');
+    well.setAttribute('class', 'well');
+    const info1 = document.createElement('h5');
+    const text1 = document.createTextNode('Tipo:' + type );
+    const info2 = document.createElement('h5');
+    const text2 = document.createTextNode('Habilidad-1:' + ability);
+    const info3 = document.createElement('h5');
+    const text3 = document.createTextNode('Habilidad-2:' + otherability);
+    const info4 = document.createElement('h5');
+    const text4 = document.createTextNode('Experiencia:' + experience);
+
 
     //agregando elementos a sus padres.
     insert.appendChild(figure);
+    view.appendChild(more)
+    collapse.appendChild(well)
     figure.appendChild(newImage);
     figure.appendChild(figcaption);
     figcaption.appendChild(named);
-    
+    insert.appendChild(view);
+    insert.appendChild(collapse);
+    info1.appendChild(text1);
+    well.appendChild(info1);
+    info2.appendChild(text2);
+    well.appendChild(info2);
+    info3.appendChild(text3);
+    well.appendChild(info3);
+    info4.appendChild(text4);
+    well.appendChild(info4);
 };
     
 
